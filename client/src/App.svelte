148ec325868c837abd6d06b3557158eeed17f353
physics3D <kroener.tobias@t-online.de>
2021-05-02 13:07:27 +0200

    add code from socket io chat example

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,87 @@
 <script lang="ts">
-	export let name: string;
+  import { io } from "socket.io-client";
+
+  let socket = io("http://localhost:3000");
+
+  let input: string;
+  let messages = [];
+
+  let submit = () => {
+    if (input) {
+      socket.emit("chat message", input);
+      input = "";
+    }
+  };
+
+  socket.on("chat message", (msg) => {
+    messages.push(msg);
+    // to update svelte
+    messages = messages;
+  });
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <ul id="messages">
+    {#each messages as message}
+      <li>{message}</li>
+    {/each}
+  </ul>
+
+  <form id="form" on:submit|preventDefault={submit}>
+    <input id="input" autocomplete="off" bind:value={input} />
+    <button>Send</button>
+  </form>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    margin: 0;
+    padding-bottom: 3rem;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Helvetica, Arial, sans-serif;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  #form {
+    background: rgba(0, 0, 0, 0.15);
+    padding: 0.25rem;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    height: 3rem;
+    box-sizing: border-box;
+    backdrop-filter: blur(10px);
+  }
+  #input {
+    border: none;
+    padding: 0 1rem;
+    flex-grow: 1;
+    border-radius: 2rem;
+    margin: 0.25rem;
+  }
+  #input:focus {
+    outline: none;
+  }
+  #form > button {
+    background: #333;
+    border: none;
+    padding: 0 1rem;
+    margin: 0.25rem;
+    border-radius: 3px;
+    outline: none;
+    color: #fff;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  #messages {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+  }
+  #messages > li {
+    padding: 0.5rem 1rem;
+  }
+  #messages > li:nth-child(odd) {
+    background: #efefef;
+  }
 </style>
--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+/* eslint-disable no-console */
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', (socket) => {
-    socket.on('chat message', msg => {
-        io.emit('chat message', msg);
-    });
+  socket.on('chat message', (msg) => {
+    console.log(msg);
+    io.emit('chat message', msg);
+  });
 });
 
-http.listen(port, () => {
-    console.log(`Socket.IO server running at http://localhost:${port}/`);
+server.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
