c9ea98ffdc42ba07d85b57e3c504240e799e228c
physics3D <kroener.tobias@t-online.de>
2022-01-21 21:25:45 +0100

    change db driver to mongodb & add Pilotfile

--- server/index.ts
+++ server/index.ts
@@ -2,134 +2,142 @@ import socketio, { Socket } from 'socket.io';
 import express from 'express';
 import http from 'http';
 import path from 'path';
-import monk from 'monk';
 import * as argon2 from 'argon2';
+import { MongoClient } from 'mongodb';
 
-const app = express();
-app.use('/', express.static(path.join(`${__dirname}/../client/public`)));
-const server = new http.Server(app);
-const io = new socketio.Server(server);
-const port = process.env.PORT || 3000;
-const db = monk('mongodb://localhost:27017/web-chat-svelte');
+async function main() {
+  const app = express();
+  app.use('/', express.static(path.join(`${__dirname}/../client/public`)));
+  const server = new http.Server(app);
+  const io = new socketio.Server(server);
+  const port = process.env.PORT || 3000;
+  const mongoClient = new MongoClient('mongodb://127.0.0.1:27017', { useUnifiedTopology: true });
+  await mongoClient.connect();
+  console.log('successfully connected');
 
-const messages = db.get('messages');
-const users = db.get('users');
+  const db = mongoClient.db('web-chat-svelte');
 
-// index for better performance
-users.createIndex('nickname');
+  const messages = db.collection('messages');
+  const users = db.collection('users');
 
-const connected_users: string[] = [];
-const typing_users: string[] = [];
+  // index for better performance
+  users.createIndex('nickname');
 
-function system_message(text: string) {
-  const msg = {
-    author: 'system',
-    text,
-    systemMessage: true,
-  };
-  messages.insert(msg);
-  io.emit('chat message', msg);
-}
+  const connected_users: string[] = [];
+  const typing_users: string[] = [];
 
-async function verify_user(nickname: string, password: string): Promise<boolean> {
-  const user = await users.findOne({ nickname });
-  if (user === null) {
-    return false;
+  function system_message(text: string) {
+    const msg = {
+      author: 'system',
+      text,
+      systemMessage: true,
+    };
+    messages.insertOne(msg);
+    io.emit('chat message', msg);
   }
-  const password_correct = await argon2.verify(user.hashed_password, password);
-  return password_correct;
-}
 
-async function check_user_exists(nickname: string): Promise<boolean> {
-  const user = await users.findOne({ nickname });
-  return user !== null;
-}
-
-io.on('connection', (socket: Socket) => {
-  socket.on('register', async (user_data, callback) => {
-    const { nickname } = user_data;
-    const { password } = user_data;
-    const user_exists = await check_user_exists(nickname);
-    if (user_exists) {
-      callback(false);
-    } else {
-      callback(true);
-      const hashed_password = await argon2.hash(password);
-      users.insert({ nickname, hashed_password });
+  async function verify_user(nickname: string, password: string): Promise<boolean> {
+    const user = await users.findOne({ nickname });
+    if (user === null) {
+      return false;
     }
-  });
+    const password_correct = await argon2.verify(user.hashed_password, password);
+    return password_correct;
+  }
 
-  socket.on('login', async (user_data, callback) => {
-    const { nickname } = user_data;
-    const { password } = user_data;
-    const login_successful = await verify_user(nickname, password);
-    if (!login_successful) {
-      callback(false);
-    } else {
-      callback(true);
-      connected_users.push(nickname);
-      system_message(`${nickname} joined the chat`);
+  async function check_user_exists(nickname: string): Promise<boolean> {
+    const user = await users.findOne({ nickname });
+    return user !== null;
+  }
 
-      const msgs = await messages.find({});
+  io.on('connection', (socket: Socket) => {
+    socket.on('register', async (user_data, callback) => {
+      const { nickname } = user_data;
+      const { password } = user_data;
+      const user_exists = await check_user_exists(nickname);
+      if (user_exists) {
+        callback(false);
+      } else {
+        callback(true);
+        const hashed_password = await argon2.hash(password);
+        users.insertOne({ nickname, hashed_password });
+      }
+    });
 
-      socket.emit('sync', msgs);
-      socket.emit('typing sync', typing_users);
+    socket.on('login', async (user_data, callback) => {
+      const { nickname } = user_data;
+      const { password } = user_data;
+      const login_successful = await verify_user(nickname, password);
+      if (!login_successful) {
+        callback(false);
+      } else {
+        callback(true);
+        connected_users.push(nickname);
+        system_message(`${nickname} joined the chat`);
 
-      socket.on('chat message', async (msg: string) => {
-        // security so no user sends system messages
-        // systemMessage is always false
-        const public_msg = {
-          author: nickname,
-          text: msg,
-          systemMessage: false,
-        };
+        const msgs = await messages.find({}).toArray();
 
-        const inserted = await messages.insert(public_msg);
-        console.log(inserted);
-        io.emit('chat message', public_msg);
-      });
+        socket.emit('sync', msgs);
+        socket.emit('typing sync', typing_users);
 
-      socket.on('typing', () => {
-        const index = typing_users.indexOf(nickname);
-        // if not found
-        if (index < 0) {
-          typing_users.push(nickname);
+        socket.on('chat message', async (msg: string) => {
+          // security so no user sends system messages
+          // systemMessage is always false
+          const public_msg = {
+            author: nickname,
+            text: msg,
+            systemMessage: false,
+          };
+
+          const inserted = await messages.insertOne(public_msg);
+          console.log(inserted);
+          io.emit('chat message', public_msg);
+        });
+
+        socket.on('typing', () => {
+          const index = typing_users.indexOf(nickname);
+          // if not found
+          if (index < 0) {
+            typing_users.push(nickname);
+            io.emit('typing', typing_users);
+          }
+        });
+
+        socket.on('typing stop', () => {
+          const index = typing_users.indexOf(nickname);
+          typing_users.splice(index, 1);
           io.emit('typing', typing_users);
-        }
-      });
+        });
 
-      socket.on('typing stop', () => {
-        const index = typing_users.indexOf(nickname);
-        typing_users.splice(index, 1);
-        io.emit('typing', typing_users);
-      });
+        socket.on('disconnect', async () => {
+          system_message(`${nickname} left the chat`);
 
-      socket.on('disconnect', async () => {
-        system_message(`${nickname} left the chat`);
+          // remove user from typing ones
+          let index = typing_users.indexOf(nickname);
+          typing_users.splice(index, 1);
+          index = connected_users.indexOf(nickname);
+          connected_users.splice(index, 1);
+          io.emit('typing', typing_users);
+        });
 
-        // remove user from typing ones
-        let index = typing_users.indexOf(nickname);
-        typing_users.splice(index, 1);
-        index = connected_users.indexOf(nickname);
-        connected_users.splice(index, 1);
-        io.emit('typing', typing_users);
-      });
-
-      socket.on('change password', async (password_data, password_callback) => {
-        const { old_password } = password_data;
-        const { new_password } = password_data;
-        const hashed_password = await argon2.hash(new_password);
-        if (await verify_user(nickname, old_password)) {
-          await users.findOneAndUpdate({ nickname }, { $set: { hashed_password } });
-          password_callback(true);
-        } else {
-          password_callback(false);
-        }
-      });
-    }
+        socket.on('change password', async (password_data, password_callback) => {
+          const { old_password } = password_data;
+          const { new_password } = password_data;
+          const hashed_password = await argon2.hash(new_password);
+          if (await verify_user(nickname, old_password)) {
+            await users.findOneAndUpdate({ nickname }, { $set: { hashed_password } });
+            password_callback(true);
+          } else {
+            password_callback(false);
+          }
+        });
+      }
+    });
   });
-});
 
-server.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
+  server.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+  });
+}
+
+main();
